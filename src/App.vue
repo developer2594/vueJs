528ff673725473da0bdfa63c27b3763b3431a070
ahmad <developer2594@gmail.com>
2022-08-02 20:16:54 +0200

    end filter

--- src/App.vue
+++ src/App.vue
@@ -107,10 +107,25 @@
       </section>
 
       <template v-if="tickers.length">
+        <div>
+          <!-- filter -->
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперёд
+          </button>
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          <div>Фильтр: <input v-model="filter" /></div>
+        </div>
+        <!-- ============ -->
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filtredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -213,7 +228,8 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
-      coins: []
+      page: 1,
+      filter: ""
     };
   },
 
@@ -227,23 +243,15 @@ export default {
     }
   },
 
-  //  mounted: function() {
-  //    const coin = {
-  //      coin: this.coins
-  //    };
-  //    this.coins.push(coin);
-  //    setTimeout(async () => {
-  //      const fu = await fetch(
-  //        `https://min-api.cryptocompare.com/data/all/coinlist?summary=true`
-  //      );
-  //      const data = await fu.json();
-  //      console.log(data);
-
-  //      console.log(this.coins);
-  //    }, 1000);
-  //  },
-
   methods: {
+    filtredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6 - 1;
+      return this.tickers
+        .filter(ticker => ticker.name.includes(this.filter))
+        .slice();
+    },
+
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -257,12 +265,14 @@ export default {
         }
       }, 5000);
     },
+
     add() {
       const currentTicker = {
         name: this.ticker,
         price: "-"
       };
       this.tickers.push(currentTicker);
+      this.filter = "";
       // save for local storeg
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
       // send currentTicker.name for setInterval
