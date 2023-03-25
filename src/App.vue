5f6274758e1dcd9bda4fc3e31b84cb44596ca17e
developer2594 <developer2594@gmail.com>
2023-03-25 15:44:24 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div
-      v-if="false"
+      v-if="load"
       class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
     >
       <svg
@@ -30,7 +30,7 @@
         <div class="flex">
           <div class="max-w-xs">
             <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер:{{ ticker }}</label
+              >Тикер:{{ ticker }}´-{{ ticker }}</label
             >
             <div class="mt-1 relative rounded-md shadow-md">
               <input
@@ -70,6 +70,7 @@
             <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
           </div>
         </div>
+        <!-- button add tickers -->
         <button
           @click="add"
           type="button"
@@ -90,12 +91,28 @@
           </svg>
           Добавить
         </button>
+        <!-- =========== -->
       </section>
       <template v-if="tickers.length">
+        <!-- filtration pagination -->
+        <div>
+          <button
+            class="mx-5 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          <button
+            class="mx-5 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперёд
+          </button>
+        </div>
+        Фильтер:<input v-model="filter" />
+        <!-- =========== filtration pagination-->
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -127,8 +144,9 @@
                   fill-rule="evenodd"
                   d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                   clip-rule="evenodd"
-                ></path></svg
-              >Удалить
+                ></path>
+              </svg>
+              Удалить
             </button>
           </div>
         </dl>
@@ -194,6 +212,13 @@ export default {
   },
 
   created() {
+    // loader
+    setTimeout(() => {
+      this.load = false;
+    }, 1000);
+    // =========== loader
+
+    // get from local storage
     const tackersData = localStorage.getItem("cryptonomicon-list");
     if (tackersData) {
       this.tickers = JSON.parse(tackersData);
@@ -201,9 +226,16 @@ export default {
         this.subscribeToUpdates(ticker.name);
       });
     }
+    // ============= get from local storage
   },
 
   methods: {
+    // filtration und pagination
+    filteredTickers() {
+      return this.tickers.filter((ticker) => ticker.name.includes(this.filter));
+    },
+
+    // updates ticker prise
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -221,33 +253,39 @@ export default {
         }
       }, 5000);
     },
+    // ============= updates ticker prise
+
+    // add new ticker
     add() {
       const currentTicker = {
-        name: this.ticker.toUpperCase(),
+        name: this.ticker,
         price: "-",
       };
 
       // add new ticker in currentTicker
       this.tickers.push(currentTicker);
-
+      // add to localStorage
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+      // sent to subscribeToUpdates currentTicker.name
       this.subscribeToUpdates(currentTicker.name);
 
       this.ticker = "";
+      this.filter = "";
     },
-
+    // ============== add new ticker
     // seleck ticker
     select(ticker) {
       this.sel = ticker;
       this.graph = [];
     },
-
+    // ============ seleck ticker
     // delete cart
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
       this.sel = null;
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
     },
-
+    // ============ delete cart
     // normalize graph
     normalizeGraph() {
       const maxValue = Math.max(...this.graph);
@@ -256,6 +294,7 @@ export default {
         (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
     },
+    // ============ normalize graph
   },
   // coin list https://min-api.cryptocompare.com/data/all/coinlist?summary=true
 };
